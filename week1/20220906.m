1071e1ec60c445991dfd22a4c990440e62348ad0
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2022-09-06 14:37:42 +0900

    Update 20220906.m

--- week1/20220906.m
+++ week1/20220906.m
@@ -1,2 +1,11 @@
 20220906.m
 1번째 수업
+PaLM
+
+강의 중)
+There are also other concerns
+1) cloud issues : if we store the model on cloud, we need to care about privacy, communication cost, latency from queues.
+2) democratization : AI is a mean of production - too harmful to be monopolized.
+(should AI access also be considered a "human right"? : AI 접근은 인권을 고려해야 하는가?)
+3) Hardware Lottery : Bad research ideas prevail only because they are provided large - computing resources(resulting in a good absolute performance.)
+: 단지 그들이 대규모 컴퓨팅 자원을 제공받기 때문에, 나쁜 연구(bad research ideas) 아이디어들이 만연하다.
